f9b6400c0d486eb94ba46c5bd76828151c08fc5b
kalexG <kevalexgomez@gmail.com>
2018-09-30 10:31:12 -0600

    added to readResistor tool

--- scripts/python/readResistor.py
+++ scripts/python/readResistor.py
@@ -2,31 +2,83 @@
 
 import sys
 import argparse
+import warnings
 
 def parseArguments(args):
     parser = argparse.ArgumentParser(description = "Decode resistor bands")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbose", action = "store_true", help = "increase output verbosity")
-    group.add_argument("-q", "--quiet", action = "store_true", help = "decrease output verbosity")
-    parser.add_argument('-b', '--bands', nargs = "+", help = "color of bands (number of bands = 2 or 3)")
-    parser.add_argument('-m', '--multiplier', help = "color of multipler")
-    parser.add_argument('-t', '--tolerance', help = "color of tolerance")
-    return parser.parse_args()
+    group.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    group.add_argument("-q", "--quiet", action="store_true", help="decrease output verbosity")
+    parser.add_argument('-d', '--digits', nargs= '+', action=argumentRange(2,3),  help="color of digit bands (number of digit bands = 2 or 3)")
+    parser.add_argument('-m', '--multiplier', nargs=1,  help="color of multipler band")
+    parser.add_argument('-t', '--tolerance', nargs=1, help="color of tolerance band")
+    parsedArgs = parser.parse_args(args)
+    return parsedArgs
 
-def raiseExceptionsWarnings():
-    print("raiseExceptionsWarnings")
+def argumentRange(minA, maxA):
+    class argumentRangeError(argparse.Action):
+        def __call__(self, parser, args, values, option_string=None):
+            if not minA<=len(values)<=maxA:
+                msg='argument "{}" requires between {} and {} arguments'.format(self.dest,minA,maxA)
+                raise argparse.ArgumentTypeError(msg)
+            setattr(args, self.dest, values)
+    return argumentRangeError
 
-def decodeResistance():
-    print("decodeResistance")
+def warningMultiplerTolerance(mul, tol):
+    if mul == None:
+        warnings.warn("You didn't provide a multipler as an input.", UserWarning)
+    if tol == None:
+        warnings.warn("You didn't provide a tolerance as an input.", UserWarning)
+
+def exceptionNonIntegerArguments():
+    print("exceptionNonIntegerArguments")
+
+def decodeResistance(dig, mul, tol):
+    colorDict = { 
+        "black"  : [ 0,      1,         "none"  ],
+        "brown"  : [ 1,      10,        "1%"    ],
+        "red"    : [ 2,      100,       "2%"    ],
+        "orange" : [ 3,      1000,      "none"  ],
+        "yellow" : [ 4,      10000,     "none"  ],
+        "green"  : [ 5,      100000,    "0.5%"  ],
+        "blue"   : [ 6,      1000000,   "0.25%" ],
+        "violet" : [ 7,      10000000,  "0.1%"  ],
+        "grey"   : [ 8,      "none",    "0.05%" ],
+        "white"  : [ 9,      "none",    "none"  ],
+        "gold"   : [ "none", 0.1,       "5%"    ],
+        "silver" : [ "none", 0.01,      "10%"   ]
+    }
+    listBands = dig + mul + tol
+    digitsStr = ""
+    if all(elem in list(colorDict.keys()) for elem in listBands):
+        for band in dig:
+            if colorDict[band][0] != "none":
+                digitsStr = digitsStr + str(colorDict[band][0])
+            else:
+                print("EXCEPTION: digits invalid")
+                return
+        resistance = int(digitsStr)
+        if colorDict[mul[0]][1] != "none":
+            resistance *= colorDict[mul[0]][1]
+        else:
+            print("EXCEPTION: multiplier invalid")
+            return
+        if colorDict[tol[0]][2] != "none":
+            tolerance = colorDict[tol[0]][2]
+        else:
+            print("EXCEPTION: tolerance invalid")
+            return
+    else:
+        print("EXCEPTION: colors invalid")
+        return
+    return [resistance, tolerance]
 
 def main():
     print("readResistor!")
     args = sys.argv[1:]
-    print(args)
     parser = parseArguments(args)
-    print(parser.bands)
-    print(parser.multiplier)
-    print(parser.tolerance)
-
+    warningMultiplerTolerance(parser.multiplier,parser.tolerance)
+    decodedResistance = decodeResistance(parser.digits, parser.multiplier, parser.tolerance)
+    print(decodedResistance)
 if __name__ == "__main__":
    main()
--- scripts/python/unitTest/readResistor_ut.py
+++ scripts/python/unitTest/readResistor_ut.py
@@ -5,50 +5,82 @@ import sys
 sys.path.insert(0, '/home/kalexg/Documents/engDevTools/scripts/python')
 import readResistor
 
-class TestDictionaryParseMethods(unittest.TestCase):
+class TestArgumentParseMethods(unittest.TestCase):
         
-    def testReadFirstBand(self):
-        self.assertTrue(False)
+    def testStoreTwoDigitBands(self):
+        parser = readResistor.parseArguments(['-d', 'red', 'blue'])
+        self.assertEqual(parser.digits, ['red', 'blue'])
+        parser = readResistor.parseArguments(['--digits', 'blue', 'yellow'])
+        self.assertEqual(parser.digits, ['blue', 'yellow'])
     
-    def testReadSecondBand(self):
-        self.assertTrue(False)
+    def testStoreThreeDigitBands(self):
+        parser = readResistor.parseArguments(['-d', 'red', 'blue', 'green'])
+        self.assertEqual(['red', 'blue', 'green'], parser.digits)
+        parser = readResistor.parseArguments(['--digits', 'blue', 'green', 'yellow'])
+        self.assertEqual(['blue', 'green', 'yellow'], parser.digits)
 
-    def testReadThirdBand(self):
-        self.assertTrue(False)
+    def testStoreMultiplerBand(self):
+        parser = readResistor.parseArguments(['-m', '10'])
+        self.assertEqual(parser.multiplier, ['10'])
+        parser = readResistor.parseArguments(['--multiplier', '11'])
+        self.assertEqual(parser.multiplier, ['11'])
 
-    def testReadMultiplier(self):
-        self.assertTrue(False)
+    def testStoreCombination(self):
+        parser = readResistor.parseArguments(['-d', '1', '2', '3', '-m', '4', '-t', '5', '-q' ])
+        self.assertEqual(parser.digits, ['1', '2', '3'])
+        self.assertEqual(parser.multiplier, ['4'])
+        self.assertEqual(parser.tolerance, ['5'])
+        self.assertEqual(parser.quiet, True)
+        self.assertEqual(parser.verbose, False)
+        parser = readResistor.parseArguments(['--digits', '2', '3', '4', '--multiplier', '5', '--tolerance', '6', '--verbose' ])
+        self.assertEqual(parser.digits, ['2', '3', '4'])
+        self.assertEqual(parser.multiplier, ['5'])
+        self.assertEqual(parser.tolerance, ['6'])
+        self.assertEqual(parser.quiet, False)
+        self.assertEqual(parser.verbose, True)
 
-    def testReadTolerance(self):
-        self.assertTrue(False)
+    def testStoreToleranceBand(self):
+        parser = readResistor.parseArguments(['-t', '5'])
+        self.assertEqual(parser.tolerance, ['5'])
+        parser = readResistor.parseArguments(['--tolerance', '6'])
+        self.assertEqual(parser.tolerance, ['6'])
 
-    def testNoThirdBand(self):
-        self.assertTrue(False)
+    def testVerbose(self):
+        parser = readResistor.parseArguments(['-v'])
+        self.assertTrue(parser.verbose)
+        parser = readResistor.parseArguments(['--verbose'])
+        self.assertTrue(parser.verbose)
 
-class TestResistorDerivation(unittest.TestCase):
+    def testQuiet(self):
+        parser = readResistor.parseArguments(['-q'])
+        self.assertTrue(parser.quiet)
+        parser = readResistor.parseArguments(['--quiet'])
+        self.assertTrue(parser.quiet)
 
-    def testThreeBand(self):
-        self.assertTrue(False)
+#class TestResistorDerivation(unittest.TestCase):
 
-    def testTwoBand(self):
-        self.assertTrue(False)
+#    def testThreeBand(self):
+#        self.assertTrue(False)
 
-class TestExceptions(unittest.TestCase):
+#    def testTwoBand(self):
+#        self.assertTrue(False)
+
+#class TestExceptions(unittest.TestCase):
         
-    def testNoFirstBandInputError(self):
-        self.assertTrue(False)
+#    def testNoFirstBandInputError(self):
+#        self.assertTrue(False)
 
-    def testNoSecondBandInputError(self):
-        self.assertTrue(False)
+#    def testNoSecondBandInputError(self):
+#        self.assertTrue(False)
 
-    def testNoMultiplierError(self):
-        self.assertTrue(False)
+#    def testNoMultiplierError(self):
+#        self.assertTrue(False)
 
-    def testNoToleranceWarning(self):
-        self.assertTrue(False)
+#    def testNoToleranceWarning(self):
+#        self.assertTrue(False)
 
-    def testMultipleExceptions(self):
-        self.assertTrue(False)
+#    def testMultipleExceptions(self):
+#        self.assertTrue(False)
 
 if __name__ == "__main__":
     unittest.main()
