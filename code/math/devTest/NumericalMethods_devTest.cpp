242ddc5a1dd0a2dd062e927dafe86c86017b2258
kalexG <kevalexgomez@gmail.com>
2022-03-25 19:01:18 -0600

    Cleanup numerical methods namespace and tests

--- code/math/devTest/NumericalMethods_devTest.cpp
+++ code/math/devTest/NumericalMethods_devTest.cpp
@@ -11,90 +11,53 @@
 #include <limits>
 #include "../inc/NumericalMethods.h"
 
-double function_1(double x)
-{
-    double f_x = pow(x,3) + 4 * pow(x, 2) - 10;
-    return f_x;
-}
-
-double function_1p(double x)
-{
-    double f_x = 3 * pow(x,2) + 8 * x;
-    return f_x;
-}
-
-double function_1pp(double x)
-{
-    double f_x = 6 * x + 8;
-    return f_x;
-}
-
-double function_2(double x)
-{
-    //double f_x = sqrt(2/(pow(x,2)-3));  
-    double f_x = pow( 3 * (pow(x,2) + 1 ), 0.25);
-    //double f_x = sqrt((pow(x,4)-3)/3);
-    return f_x;
-}
-
-double function_3(double x)
-{
-    double f_x = cos(x) - x;
-    return f_x;
-}
-
-double function_3p(double x)
-{
-    double fp_x = -sin(x) - 1;
-    return fp_x;
-}
-
-double function_4(double x)
-{
-    double f_x = 0.5 * (x + (3/x));
-    return f_x;
-}
+double function_1(double x)   { return pow(x,3) + 4 * pow(x, 2) - 10; }
+double function_1p(double x)  { return 3 * pow(x,2) + 8 * x; }
+double function_1pp(double x) { return 6 * x + 8; }
+double function_2(double x)   { return pow( 3 * (pow(x,2) + 1 ), 0.25); }
+double function_3(double x)   { return cos(x) - x; }
+double function_3p(double x)  { return  -sin(x) - 1; }
+double function_4(double x)   { return 0.5 * (x + (3/x)); }
 
 int main( int argc, char *argv[] ) {
 
-    using namespace NumericalMethods;
     // Inputs
-    printf("You made it to test function for NumericalMethods!\n");
-    std::vector<NumericalMethods::rootSolver_ret> ans1, ans2, ans3, ans4, ans5, ans6, ans7;
+    printf("You made it to test function for NumericalMethods!\n\n");
+    std::vector<NumericalMethods::RootSolvers::rootSolver_ret> ans1, ans2, ans3, ans4, ans5, ans6, ans7;
 
     printf("NM Bisection Method Test\n");
-    ans1 = NumericalMethods::bisection_method(&function_1, 1, 2, 1e-04);
-    std::string res1 = NumericalMethods::rootSolver_results(ans1);
+    ans1 = NumericalMethods::RootSolvers::bisection_method(&function_1, 1, 2, 1e-04);
+    std::string res1 = NumericalMethods::RootSolvers::rootSolver_results(ans1);
     printf("%s\n", res1.c_str());
     
     printf("NM Fixed-Point Iteration Method Test\n");
-    ans2 = NumericalMethods::fixedPointIteration_method(&function_2, 1, 1e-02);
-    std::string res2 = NumericalMethods::rootSolver_results(ans2);
+    ans2 = NumericalMethods::RootSolvers::fixedPointIteration_method(&function_2, 1, 1e-02);
+    std::string res2 = NumericalMethods::RootSolvers::rootSolver_results(ans2);
     printf("%s\n", res2.c_str());
     
     printf("NM Newton's Method Test\n");
-    ans3 = NumericalMethods::newtons_method(&function_3, &function_3p, M_PI/4.0, 1e-08, 20);
-    std::string res3 = NumericalMethods::rootSolver_results(ans3);
+    ans3 = NumericalMethods::RootSolvers::newtons_method(&function_3, &function_3p, M_PI/4.0, 1e-08, 20);
+    std::string res3 = NumericalMethods::RootSolvers::rootSolver_results(ans3);
     printf("%s\n", res3.c_str());
     
     printf("NM Secant Method Test\n");
-    ans4 = NumericalMethods::secant_method(&function_3, 0.5, M_PI/4.0, 1e-08, 20);
-    std::string res4 = NumericalMethods::rootSolver_results(ans4);
+    ans4 = NumericalMethods::RootSolvers::secant_method(&function_3, 0.5, M_PI/4.0, 1e-08, 20);
+    std::string res4 = NumericalMethods::RootSolvers::rootSolver_results(ans4);
     printf("%s\n", res4.c_str());
     
     printf("NM Method of False Position Test\n");
-    ans5 = NumericalMethods::falsePosition_method(&function_3, 0.5, M_PI/4.0, 1e-08, 20);
-    std::string res5 = NumericalMethods::rootSolver_results(ans5);
+    ans5 = NumericalMethods::RootSolvers::falsePosition_method(&function_3, 0.5, M_PI/4.0, 1e-08, 20);
+    std::string res5 = NumericalMethods::RootSolvers::rootSolver_results(ans5);
     printf("%s\n", res5.c_str());
     
     printf("NM Modified Newton's Method Test\n");
-    ans6 = NumericalMethods::ModifiedNewtons_method(&function_1, &function_1p, &function_1pp, 1.5, 1e-10, 20);
-    std::string res6 = NumericalMethods::rootSolver_results(ans6);
+    ans6 = NumericalMethods::RootSolvers::ModifiedNewtons_method(&function_1, &function_1p, &function_1pp, 1.5, 1e-10, 20);
+    std::string res6 = NumericalMethods::RootSolvers::rootSolver_results(ans6);
     printf("%s\n", res6.c_str());
     
     printf("NM Steffenson's Method Test\n");
-    ans7 = NumericalMethods::steffensens_method(&function_4, 0.5, 1e-04);
-    std::string res7 = NumericalMethods::rootSolver_results(ans7);
+    ans7 = NumericalMethods::RootSolvers::steffensens_method(&function_4, 0.5, 1e-04);
+    std::string res7 = NumericalMethods::RootSolvers::rootSolver_results(ans7);
     printf("%s\n", res7.c_str());
 
     return 0;
--- code/math/inc/NumericalMethods.h
+++ code/math/inc/NumericalMethods.h
@@ -18,279 +18,282 @@
 #pragma once
 namespace NumericalMethods
 {
-    // Return struct for root solvers
-    struct rootSolver_ret
+    namespace RootSolvers
     {
-        uint32_t n;
-        double   p_n;
-    };
-
-    // Bisection Method
-    inline std::vector<rootSolver_ret> bisection_method(std::function<double(double)> f_x, 
-                                                        double a, 
-                                                        double b, 
-                                                        double tol = 1e-05, 
-                                                        uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;    
-        double f_a = f_x(a);
-        double f_p_n;
-        // First entry 
-        entry.n = 0;
-        entry.p_n = b-a;
-        ret.push_back(entry);
-        uint32_t n = 1;
-        while ( n <= max_it )
+        // Return struct for root solvers
+        struct rootSolver_ret
         {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = a + ( b - a ) / 2;
-            f_p_n = f_x(entry.p_n);
-            ret.push_back(entry);
-
-            if ( f_p_n == 0 || ( b - a ) / 2 < tol )
-            {
-                return ret;
-            }
-
-            if ( f_a * f_p_n > 0 )
-            {
-                a = entry.p_n;
-                f_a = f_p_n;
-            }
-            else
-            {
-                b = entry.p_n;
-            }
-            
-            n++;
-        }
-        return ret;   
-    };
-
-    // Fixed-Point Iteration Method
-    inline std::vector<rootSolver_ret> fixedPointIteration_method(std::function<double(double)> f_x, 
-                                                                  double p_0, 
-                                                                  double tol = 1e-05, 
-                                                                  uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;    
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        uint32_t n = 1;
-        while ( n <= max_it )
+            uint32_t n;
+            double   p_n;
+        };
+        
+        inline std::string rootSolver_results(std::vector<rootSolver_ret> ans)
         {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = f_x(p_0);
-            ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_0 ) < tol )
+            std::ostringstream tmpStream;
+            for ( uint32_t i = 0; i < ans.size(); i++)
             {
-                return ret;
+                tmpStream << "n = "     << ans[ i ].n     << " | " <<
+                            "p_n = "   << ans[ i ].p_n << " |\n";
             }
+            std::string tmpString = tmpStream.str();
+            return tmpString;
+        };
 
-            n++;
-            p_0 = entry.p_n;
-        }
-        return ret;
-    };
-
-    // Netwon's Method
-    inline std::vector<rootSolver_ret> newtons_method(std::function<double(double)> f_x, 
-                                                      std::function<double(double)> fp_x,
-                                                      double p_0, 
-                                                      double tol = 1e-05, 
-                                                      uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;    
-        uint32_t n = 1;
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        while ( n <= max_it )
-        {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = p_0 - f_x(p_0) / fp_x(p_0);
-            ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_0) < tol )
-            {
-                return ret;
-            }
-            p_0 = entry.p_n;
-            n++;
-        }
-        return ret;   
-    };
-    
-    // Secant Method
-    inline std::vector<rootSolver_ret> secant_method(std::function<double(double)> f_x, 
-                                                     double p_0, 
-                                                     double p_1, 
-                                                     double tol = 1e-05, 
-                                                     uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;   
-        uint32_t n = 2;
-        double q_0 = f_x(p_0);
-        double q_1 = f_x(p_1);
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        // Second entry 
-        entry.n = 1;
-        entry.p_n = p_1;
-        ret.push_back(entry);
-        while ( n <= max_it )
-        {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = p_1 - q_1 * (p_1 - p_0) / (q_1 - q_0);
-            ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_1 ) < tol )
-            {
-                return ret;
-            }
-
-            p_0 = p_1;
-            q_0 = q_1;
-            p_1 = entry.p_n;
-            q_1 = f_x(entry.p_n);
-            n++;
-        }
-        return ret;   
-    };
-
-    // Method of False Position
-    inline std::vector<rootSolver_ret> falsePosition_method(std::function<double(double)> f_x, 
-                                                            double p_0, 
-                                                            double p_1, 
+        // Bisection Method
+        inline std::vector<rootSolver_ret> bisection_method(std::function<double(double)> f_x, 
+                                                            double a, 
+                                                            double b, 
                                                             double tol = 1e-05, 
                                                             uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;   
-        uint32_t n = 2;
-        double q_0 = f_x(p_0);
-        double q_1 = f_x(p_1);
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        // Second entry 
-        entry.n = 1;
-        entry.p_n = p_1;
-        ret.push_back(entry);
-        while ( n <= max_it )
         {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = p_1 - q_1 * (p_1 - p_0) / (q_1 - q_0);
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;    
+            double f_a = f_x(a);
+            double f_p_n;
+            // First entry 
+            entry.n = 0;
+            entry.p_n = b-a;
             ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_1 ) < tol )
+            uint32_t n = 1;
+            while ( n <= max_it )
             {
-                return ret;
+                // Store metadata
+                entry.n = n;
+                entry.p_n = a + ( b - a ) / 2;
+                f_p_n = f_x(entry.p_n);
+                ret.push_back(entry);
+
+                if ( f_p_n == 0 || ( b - a ) / 2 < tol )
+                {
+                    return ret;
+                }
+
+                if ( f_a * f_p_n > 0 )
+                {
+                    a = entry.p_n;
+                    f_a = f_p_n;
+                }
+                else
+                {
+                    b = entry.p_n;
+                }
+                
+                n++;
             }
+            return ret;   
+        };
 
-            if (f_x(entry.p_n) * q_1 < 0)
+        // Fixed-Point Iteration Method
+        inline std::vector<rootSolver_ret> fixedPointIteration_method(std::function<double(double)> f_x, 
+                                                                    double p_0, 
+                                                                    double tol = 1e-05, 
+                                                                    uint32_t max_it = std::numeric_limits<uint32_t>::max() )
+        {
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;    
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
+            ret.push_back(entry);
+            uint32_t n = 1;
+            while ( n <= max_it )
             {
+                // Store metadata
+                entry.n = n;
+                entry.p_n = f_x(p_0);
+                ret.push_back(entry);
+
+                if ( fabs( entry.p_n - p_0 ) < tol )
+                {
+                    return ret;
+                }
+
+                n++;
+                p_0 = entry.p_n;
+            }
+            return ret;
+        };
+
+        // Netwon's Method
+        inline std::vector<rootSolver_ret> newtons_method(std::function<double(double)> f_x, 
+                                                        std::function<double(double)> fp_x,
+                                                        double p_0, 
+                                                        double tol = 1e-05, 
+                                                        uint32_t max_it = std::numeric_limits<uint32_t>::max() )
+        {
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;    
+            uint32_t n = 1;
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
+            ret.push_back(entry);
+            while ( n <= max_it )
+            {
+                // Store metadata
+                entry.n = n;
+                entry.p_n = p_0 - f_x(p_0) / fp_x(p_0);
+                ret.push_back(entry);
+
+                if ( fabs( entry.p_n - p_0) < tol )
+                {
+                    return ret;
+                }
+                p_0 = entry.p_n;
+                n++;
+            }
+            return ret;   
+        };
+        
+        // Secant Method
+        inline std::vector<rootSolver_ret> secant_method(std::function<double(double)> f_x, 
+                                                        double p_0, 
+                                                        double p_1, 
+                                                        double tol = 1e-05, 
+                                                        uint32_t max_it = std::numeric_limits<uint32_t>::max() )
+        {
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;   
+            uint32_t n = 2;
+            double q_0 = f_x(p_0);
+            double q_1 = f_x(p_1);
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
+            ret.push_back(entry);
+            // Second entry 
+            entry.n = 1;
+            entry.p_n = p_1;
+            ret.push_back(entry);
+            while ( n <= max_it )
+            {
+                // Store metadata
+                entry.n = n;
+                entry.p_n = p_1 - q_1 * (p_1 - p_0) / (q_1 - q_0);
+                ret.push_back(entry);
+
+                if ( fabs( entry.p_n - p_1 ) < tol )
+                {
+                    return ret;
+                }
+
                 p_0 = p_1;
                 q_0 = q_1;
+                p_1 = entry.p_n;
+                q_1 = f_x(entry.p_n);
+                n++;
             }
+            return ret;   
+        };
 
-            p_1 = entry.p_n;
-            q_1 = f_x(entry.p_n);
-            n++;
-        }
-        return ret;   
-    };
-    
-    inline std::string rootSolver_results(std::vector<rootSolver_ret> ans)
-    {
-        std::ostringstream tmpStream;
-        for ( uint32_t i = 0; i < ans.size(); i++)
+        // Method of False Position
+        inline std::vector<rootSolver_ret> falsePosition_method(std::function<double(double)> f_x, 
+                                                                double p_0, 
+                                                                double p_1, 
+                                                                double tol = 1e-05, 
+                                                                uint32_t max_it = std::numeric_limits<uint32_t>::max() )
         {
-            tmpStream << "n = "     << ans[ i ].n     << " | " <<
-                         "p_n = "   << ans[ i ].p_n << " |\n";
-        }
-        std::string tmpString = tmpStream.str();
-        return tmpString;
-    };
-
-    // Modified Netwon's Method
-    inline std::vector<rootSolver_ret> ModifiedNewtons_method(std::function<double(double)> f_x, 
-                                                              std::function<double(double)> fp_x,
-                                                              std::function<double(double)> fpp_x,
-                                                              double p_0, 
-                                                              double tol = 1e-05, 
-                                                              uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;    
-        uint32_t n = 1;
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        while ( n <= max_it )
-        {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = p_0 - (f_x(p_0) * fp_x(p_0)) / (pow(fp_x(p_0),2) - f_x(p_0) * fpp_x(p_0));
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;   
+            uint32_t n = 2;
+            double q_0 = f_x(p_0);
+            double q_1 = f_x(p_1);
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
             ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_0) < tol )
-            {
-                return ret;
-            }
-            p_0 = entry.p_n;
-            n++;
-        }
-        return ret;   
-    };
-    
-    // Steffensen's Method
-    inline std::vector<rootSolver_ret> steffensens_method(std::function<double(double)> f_x, 
-                                                          double p_0, 
-                                                          double tol = 1e-05, 
-                                                          uint32_t max_it = std::numeric_limits<uint32_t>::max() )
-    {
-        std::vector<rootSolver_ret> ret;
-        rootSolver_ret entry;    
-        // First entry 
-        entry.n = 0;
-        entry.p_n = p_0;
-        ret.push_back(entry);
-        uint32_t n = 1;
-        while ( n <= max_it )
-        {
-            // Store metadata
-            entry.n = n;
-            entry.p_n = p_0 - pow((f_x(p_0) - p_0), 2) / (f_x(f_x(p_0)) - 2 * f_x(p_0) + p_0);
+            // Second entry 
+            entry.n = 1;
+            entry.p_n = p_1;
             ret.push_back(entry);
-
-            if ( fabs( entry.p_n - p_0 ) < tol )
+            while ( n <= max_it )
             {
-                return ret;
-            }
+                // Store metadata
+                entry.n = n;
+                entry.p_n = p_1 - q_1 * (p_1 - p_0) / (q_1 - q_0);
+                ret.push_back(entry);
 
-            n++;
-            p_0 = entry.p_n;
-        }
-        return ret;
-    };
+                if ( fabs( entry.p_n - p_1 ) < tol )
+                {
+                    return ret;
+                }
+
+                if (f_x(entry.p_n) * q_1 < 0)
+                {
+                    p_0 = p_1;
+                    q_0 = q_1;
+                }
+
+                p_1 = entry.p_n;
+                q_1 = f_x(entry.p_n);
+                n++;
+            }
+            return ret;   
+        };
+        
+        // Modified Netwon's Method
+        inline std::vector<rootSolver_ret> ModifiedNewtons_method(std::function<double(double)> f_x, 
+                                                                std::function<double(double)> fp_x,
+                                                                std::function<double(double)> fpp_x,
+                                                                double p_0, 
+                                                                double tol = 1e-05, 
+                                                                uint32_t max_it = std::numeric_limits<uint32_t>::max() )
+        {
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;    
+            uint32_t n = 1;
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
+            ret.push_back(entry);
+            while ( n <= max_it )
+            {
+                // Store metadata
+                entry.n = n;
+                entry.p_n = p_0 - (f_x(p_0) * fp_x(p_0)) / (pow(fp_x(p_0),2) - f_x(p_0) * fpp_x(p_0));
+                ret.push_back(entry);
+
+                if ( fabs( entry.p_n - p_0) < tol )
+                {
+                    return ret;
+                }
+                p_0 = entry.p_n;
+                n++;
+            }
+            return ret;   
+        };
+        
+        // Steffensen's Method
+        inline std::vector<rootSolver_ret> steffensens_method(std::function<double(double)> f_x, 
+                                                            double p_0, 
+                                                            double tol = 1e-05, 
+                                                            uint32_t max_it = std::numeric_limits<uint32_t>::max() )
+        {
+            std::vector<rootSolver_ret> ret;
+            rootSolver_ret entry;    
+            // First entry 
+            entry.n = 0;
+            entry.p_n = p_0;
+            ret.push_back(entry);
+            uint32_t n = 1;
+            while ( n <= max_it )
+            {
+                // Store metadata
+                entry.n = n;
+                entry.p_n = p_0 - pow((f_x(p_0) - p_0), 2) / (f_x(f_x(p_0)) - 2 * f_x(p_0) + p_0);
+                ret.push_back(entry);
+
+                if ( fabs( entry.p_n - p_0 ) < tol )
+                {
+                    return ret;
+                }
+
+                n++;
+                p_0 = entry.p_n;
+            }
+            return ret;
+        };
+    }
 }
 
 #endif
